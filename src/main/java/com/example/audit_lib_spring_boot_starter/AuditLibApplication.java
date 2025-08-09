d4a30add0d5dd9f0a40b0b29a8b488478fbf076e
Nikita Borisov <nikita.borisov44444@gmail.com>
2025-08-09 11:25:58 +0400

    chore: init commit

---
+++ src/main/java/com/example/audit_lib_spring_boot_starter/AuditLibApplication.java
@@ -0,0 +1,13 @@
+package com.example.audit_lib_spring_boot_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditLibApplication.class, args);
+	}
+
+}
